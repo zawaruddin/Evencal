ac41e845c91c54a0691a008777d36c0529def68c
Septian Enggar S <septianenggars@Septians-MacBook-Pro.local>
2021-10-26 07:47:31 +0700

    Uupdate hacktoberfest new branch

--- application/config/database.php
+++ application/config/database.php
@@ -50,7 +50,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = '';
+$db['default']['password'] = 'root';
 $db['default']['database'] = 'evencal';
 $db['default']['dbdriver'] = 'mysqli';
 $db['default']['dbprefix'] = '';
